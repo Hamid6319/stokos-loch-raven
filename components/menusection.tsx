aeb599d56cae913e28662beaea140df7ae092d72
Hamid Ali Abbasi <hamidaliabbasi6319@gmail.com>
2026-05-01 12:00:29 -0700

    updated

--- components/menusection.tsx
+++ components/menusection.tsx
@@ -35,7 +35,7 @@ export default function MenuSection({ title, products }: MenuSectionProps) {
       </div>
 
       {/* Grid */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 md:gap-8">
+      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-8">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
