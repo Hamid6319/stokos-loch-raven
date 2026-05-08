07c2aa5fbccf713365f6de4cdccd6f5538e7f8ef
Hamid Ali Abbasi <hamidaliabbasi6319@gmail.com>
2026-05-08 09:03:43 -0700

    updated flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
+// import MainWebsite from "./mainwebsite";
+
+// export default function Page() {
+//   // redirect("/store/towson");
+
+//   return <MainWebsite />;
+// }
+
+
 import MainWebsite from "./mainwebsite";
 
 export default function Page() {
-  // redirect("/store/towson");
-
   return <MainWebsite />;
 }
